4dae27439c900f850e44af92a520b48cdd4e8561
NaijasonEzeiru <67031753+NaijasonEzeiru@users.noreply.github.com>
2023-08-16 06:31:07 +0100

    init

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+const PORT = process.env.PORT || 4000;
+
+app.use(
+  cors({
+    origin: [
+      'http://127.0.0.1:5500',
+      'http://localhost',
+      'dynamic-total-services.onrender.com'
+    ],
+    credentials: true
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Email Forward Backend</h1>');
+});
+app.post('/dts', (req, res) => {
+  console.log(req.body);
+  const html = Object.keys(req.body)?.map((key, index) => {
+    return `<h3>${key}:</h3> <p>${req.body[key]}</p>`;
+  });
+  console.log(html.join(' '));
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: { user: 'jayseehe1035@gmail.com', pass: 'sveboccesvoaxrep' }
+  });
+  var mailOptions = {
+    from: req.body.email,
+    // "DTS <email>"
+    // to: 'ezeiruchibuike@gmail.com',
+    to: 'admin@dynamictotalservices.com.au',
+    subject: req.body.subject,
+    html: `<div>${html.join(' ')}</div>`
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(info.response);
+    }
+  });
+  res.status(204);
+});
+app.listen(PORT, () => console.log('server running on port: ' + PORT));
+
+module.exports = app;
